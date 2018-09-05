e14b9439bc85da02db18bec4905c34961ccdf7f2
Yuta K <orn_tk@me.com>
2018-09-05 17:21:59 +0900

    Add golang image

---
+++ golang/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+go build ./main.go | awk '{print "go:$0";fflush()}'
+status_code=$?
+if [ ${status_code} -ne 0 ];then
+	echo "Exit Code: ${status_code}"
+	exit 0
+fi
+if [ ! -e stdout ]; then
+    mkfifo stdout
+fi
+if [ ! -e stderr ]; then
+    mkfifo stderr
+fi
+(cat < stdout) | awk '{print "stdout:"$0 > "/dev/stdout";fflush()}'&
+(cat < stderr) | awk '{print "stderr:"$0 > "/dev/stdout";fflush()}'&
+timeout 3 ./main 1> stdout 2> stderr
+status_code=$?
+rm stdout&
+rm stderr&
+if [ ${status_code} -ne 0 ];then
+	echo "Exit Code: ${status_code}"
+	exit 0
+fi
